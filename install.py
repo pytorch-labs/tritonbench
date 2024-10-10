1646235f07487ad63b4e2f396fefc11aa5079287
Xu Zhao <xzhao9@meta.com>
2024-10-10 14:42:05 -0400

    Add installation scripts.

---
+++ install.py
@@ -0,0 +1,104 @@
+import argparse
+import os
+import subprocess
+import sys
+from pathlib import Path
+
+from utils.cuda_utils import CUDA_VERSION_MAP, DEFAULT_CUDA_VERSION
+from utils.python_utils import pip_install_requirements
+
+REPO_PATH = Path(os.path.abspath(__file__)).parent.parent.parent
+FA3_PATH = REPO_PATH.joinpath("submodules", "flash-attention", "hopper")
+FBGEMM_PATH = REPO_PATH.joinpath("submodules", "FBGEMM", "fbgemm_gpu")
+
+def install_jax(cuda_version=DEFAULT_CUDA_VERSION):
+    jax_package_name = CUDA_VERSION_MAP[cuda_version]["jax"]
+    jax_nightly_html = (
+        "https://storage.googleapis.com/jax-releases/jax_nightly_releases.html"
+    )
+    # install instruction:
+    # https://jax.readthedocs.io/en/latest/installation.html
+    # pip install -U --pre jax[cuda12] -f https://storage.googleapis.com/jax-releases/jax_nightly_releases.html
+    cmd = ["pip", "install", "--pre", jax_package_name, "-f", jax_nightly_html]
+    subprocess.check_call(cmd)
+    # Test jax installation
+    test_cmd = [sys.executable, "-c", "import jax"]
+    subprocess.check_call(test_cmd)
+
+
+def install_fbgemm():
+    cmd = ["pip", "install", "-r", "requirements.txt"]
+    subprocess.check_call(cmd, cwd=str(FBGEMM_PATH.resolve()))
+    # Build target A100(8.0) or H100(9.0, 9.0a)
+    cmd = [
+        sys.executable,
+        "setup.py",
+        "install",
+        "--package_variant=genai",
+        "-DTORCH_CUDA_ARCH_LIST=8.0;9.0;9.0a",
+    ]
+    subprocess.check_call(cmd, cwd=str(FBGEMM_PATH.resolve()))
+
+
+def test_fbgemm():
+    print("Checking fbgemm_gpu installation...", end="")
+    cmd = [sys.executable, "-c", "import fbgemm_gpu.experimental.gen_ai"]
+    subprocess.check_call(cmd)
+    print("OK")
+
+
+def install_cutlass():
+    from utils.cutlass_kernels.install import install_colfax_cutlass
+    install_colfax_cutlass()
+
+
+def install_fa():
+    cmd = [sys.executable, "setup.py", "install"]
+    subprocess.check_call(cmd, cwd=str(FA3_PATH.resolve()))
+
+
+def install_liger():
+    # Liger-kernel has a conflict dependency `triton` with pytorch,
+    # so we need to install it without dependencies
+    cmd = ["pip", "install", "liger-kernel", "--no-deps"]
+    subprocess.check_call(cmd)
+
+
+def install_tk():
+    from utils.tk.install import install_tk
+    install_tk()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(allow_abbrev=False)
+    parser.add_argument("--fbgemm", action="store_true", help="Install FBGEMM GPU")
+    parser.add_argument(
+        "--cutlass", action="store_true", help="Install optional CUTLASS kernels"
+    )
+    parser.add_argument(
+        "--fa", action="store_true", help="Install optional flash_attention kernels"
+    )
+    parser.add_argument("--jax", action="store_true", help="Install jax nightly")
+    parser.add_argument("--tk", action="store_true", help="Install ThunderKittens")
+    parser.add_argument("--liger", action="store_true", help="Install Liger-kernel")
+    parser.add_argument("--all", action="store_true", help="Install all custom kernel repos")
+    parser.add_argument("--test", action="store_true", help="Run tests")
+    args = parser.parse_args()
+
+    # Install framework dependencies only
+    pip_install_requirements("requirements.txt")
+    if args.fbgemm or args.all:
+        install_fbgemm()
+    if args.fa or args.all:
+        install_fa()
+    if args.cutlass or args.all:
+        install_cutlass()
+    if args.jax or args.all:
+        install_jax()
+    if args.tk or args.all:
+        install_tk()
+    if args.liger or args.all:
+        install_liger()
+    # Run tests of the installation
+    if args.test:
+        test_fbgemm()
---
+++ utils/cuda_utils.py
@@ -0,0 +1,200 @@
+import argparse
+import importlib
+import os
+import re
+import subprocess
+from pathlib import Path
+
+from typing import Optional
+
+# defines the default CUDA version to compile against
+DEFAULT_CUDA_VERSION = "12.4"
+REPO_ROOT = Path(__file__).parent.parent
+
+CUDA_VERSION_MAP = {
+    "12.4": {
+        "pytorch_url": "cu124",
+        "magma": "magma-cuda124",
+        "jax": "jax[cuda12]",
+    },
+}
+PIN_CMAKE_VERSION = "3.22.*"
+
+TORCHBENCH_TORCH_NIGHTLY_PACKAGES = ["torch", "torchvision", "torchaudio"]
+BUILD_REQUIREMENTS_FILE = REPO_ROOT.joinpath("utils", "build_requirements.txt")
+
+
+def _nvcc_output_match(nvcc_output, target_cuda_version):
+    regex = "release (.*),"
+    version = re.search(regex, nvcc_output).groups()[0]
+    return version == target_cuda_version
+
+
+def prepare_cuda_env(cuda_version: str, dryrun=False):
+    assert (
+        cuda_version in CUDA_VERSION_MAP
+    ), f"Required CUDA version {cuda_version} doesn't exist in {CUDA_VERSION_MAP.keys()}."
+    env = os.environ.copy()
+    # step 1: setup CUDA path and environment variables
+    cuda_path = Path("/").joinpath("usr", "local", f"cuda-{cuda_version}")
+    assert (
+        cuda_path.exists() and cuda_path.is_dir()
+    ), f"Expected CUDA Library path {cuda_path} doesn't exist."
+    cuda_path_str = str(cuda_path.resolve())
+    env["CUDA_ROOT"] = cuda_path_str
+    env["CUDA_HOME"] = cuda_path_str
+    env["PATH"] = f"{cuda_path_str}/bin:{env['PATH']}"
+    env["CMAKE_CUDA_COMPILER"] = str(cuda_path.joinpath("bin", "nvcc").resolve())
+    env["LD_LIBRARY_PATH"] = (
+        f"{cuda_path_str}/lib64:{cuda_path_str}/extras/CUPTI/lib64:{env['LD_LIBRARY_PATH']}"
+    )
+    if dryrun:
+        print(f"CUDA_HOME is set to {env['CUDA_HOME']}")
+    # step 2: test call to nvcc to confirm the version is correct
+    test_nvcc = ["nvcc", "--version"]
+    if dryrun:
+        print(f"Checking nvcc version, command {test_nvcc}")
+    else:
+        output = subprocess.check_output(
+            test_nvcc, stderr=subprocess.STDOUT, env=env
+        ).decode()
+        print(f"NVCC version output: {output}")
+        assert _nvcc_output_match(
+            output, cuda_version
+        ), f"Expected CUDA version {cuda_version}, getting nvcc test result {output}"
+    # step 3: install the correct magma version
+    install_magma_cmd = [
+        "conda",
+        "install",
+        "-y",
+        "-c",
+        "pytorch",
+        CUDA_VERSION_MAP[cuda_version]["magma"],
+    ]
+    if dryrun:
+        print(f"Installing CUDA magma: {install_magma_cmd}")
+    subprocess.check_call(install_magma_cmd, env=env)
+    return env
+
+
+def setup_cuda_softlink(cuda_version: str):
+    assert (
+        cuda_version in CUDA_VERSION_MAP
+    ), f"Required CUDA version {cuda_version} doesn't exist in {CUDA_VERSION_MAP.keys()}."
+    cuda_path = Path("/").joinpath("usr", "local", f"cuda-{cuda_version}")
+    assert (
+        cuda_path.exists() and cuda_path.is_dir()
+    ), f"Expected CUDA Library path {cuda_path} doesn't exist."
+    current_cuda_path = Path("/").joinpath("usr", "local", "cuda")
+    if current_cuda_path.exists():
+        assert (
+            current_cuda_path.is_symlink()
+        ), f"Expected /usr/local/cuda to be a symlink."
+        current_cuda_path.unlink()
+    os.symlink(str(cuda_path.resolve()), str(current_cuda_path.resolve()))
+
+
+def install_pytorch_nightly(cuda_version: str, env, dryrun=False):
+    uninstall_torch_cmd = ["pip", "uninstall", "-y"]
+    uninstall_torch_cmd.extend(TORCHBENCH_TORCH_NIGHTLY_PACKAGES)
+    if dryrun:
+        print(f"Uninstall pytorch: {uninstall_torch_cmd}")
+    else:
+        # uninstall multiple times to make sure the env is clean
+        for _loop in range(3):
+            subprocess.check_call(uninstall_torch_cmd)
+    pytorch_nightly_url = f"https://download.pytorch.org/whl/nightly/{CUDA_VERSION_MAP[cuda_version]['pytorch_url']}"
+    install_torch_cmd = ["pip", "install", "--pre", "--no-cache-dir"]
+    install_torch_cmd.extend(TORCHBENCH_TORCH_NIGHTLY_PACKAGES)
+    install_torch_cmd.extend(["-i", pytorch_nightly_url])
+    if dryrun:
+        print(f"Install pytorch nightly: {install_torch_cmd}")
+    else:
+        subprocess.check_call(install_torch_cmd, env=env)
+
+
+def install_torch_deps(cuda_version: str):
+    # install magma
+    magma_pkg = CUDA_VERSION_MAP[cuda_version]["magma"]
+    cmd = ["conda", "install", "-y", magma_pkg, "-c", "pytorch"]
+    subprocess.check_call(cmd)
+    # install other dependencies
+    torch_deps = [
+        "requests",
+        "ninja",
+        "pyyaml",
+        "setuptools",
+        "gitpython",
+        "beautifulsoup4",
+        "regex",
+    ]
+    cmd = ["conda", "install", "-y"] + torch_deps
+    subprocess.check_call(cmd)
+
+
+def install_torch_build_deps(cuda_version: str):
+    install_torch_deps(cuda_version=cuda_version)
+    # Pin cmake version to stable
+    # See: https://github.com/pytorch/builder/pull/1269
+    torch_build_deps = [
+        "cffi",
+        "sympy",
+        "typing_extensions",
+        "future",
+        "six",
+        "dataclasses",
+        "tabulate",
+        "tqdm",
+        "mkl",
+        "mkl-include",
+        f"cmake={PIN_CMAKE_VERSION}",
+    ]
+    cmd = ["conda", "install", "-y"] + torch_build_deps
+    subprocess.check_call(cmd)
+    build_deps = ["ffmpeg"]
+    cmd = ["conda", "install", "-y"] + build_deps
+    subprocess.check_call(cmd)
+    # pip build deps
+    cmd = ["pip", "install", "-r"] + [str(BUILD_REQUIREMENTS_FILE.resolve())]
+    subprocess.check_call(cmd)
+    # conda forge deps
+    # ubuntu 22.04 comes with libstdcxx6 12.3.0
+    # we need to install the same library version in conda
+    conda_deps = ["libstdcxx-ng=12.3.0"]
+    cmd = ["conda", "install", "-y", "-c", "conda-forge"] + conda_deps
+    subprocess.check_call(cmd)
+
+
+def install_torchbench_deps():
+    # tritonbench flash_attn depends on packaging to build
+    cmd = ["pip", "install", "unittest-xml-reporting", "boto3", "packaging"]
+    subprocess.check_call(cmd)
+
+
+def get_torch_nightly_version(pkg_name: str):
+    pkg = importlib.import_module(pkg_name)
+    version = pkg.__version__
+    regex = ".*dev([0-9]+).*"
+    date_str = re.match(regex, version).groups()[0]
+    pkg_ver = {"version": version, "date": date_str}
+    return (pkg_name, pkg_ver)
+
+
+def check_torch_nightly_version(force_date: Optional[str] = None):
+    pkg_versions = dict(
+        map(get_torch_nightly_version, TORCHBENCH_TORCH_NIGHTLY_PACKAGES)
+    )
+    pkg_dates = list(map(lambda x: x[1]["date"], pkg_versions.items()))
+    if not len(set(pkg_dates)) == 1:
+        raise RuntimeError(
+            f"Found more than 1 dates in the torch nightly packages: {pkg_versions}."
+        )
+    if force_date and not pkg_dates[0] == force_date:
+        raise RuntimeError(
+            f"Force date value {force_date}, but found torch packages {pkg_versions}."
+        )
+    force_date_str = f"User force date {force_date}" if force_date else ""
+    print(
+        f"Installed consistent torch nightly packages: {pkg_versions}. {force_date_str}"
+    )
+
