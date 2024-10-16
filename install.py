09a57ce847c2997247a7d05fbcd3b523da1d1586
Xu Zhao <xzhao9@meta.com>
2024-10-16 13:37:10 -0400

    Skip fa2 compile in docker (#5)

--- install.py
+++ install.py
@@ -115,7 +115,8 @@ if __name__ == "__main__":
     if args.fbgemm or args.all:
         logger.info("[tritonbench] installing FBGEMM...")
         install_fbgemm()
-    if args.fa2 or args.all:
+    # TODO: for some reason, fa2 compile will break docker build
+    if args.fa2:
         logger.info("[tritonbench] installing fa2...")
         install_fa2()
     if args.fa2_compile:
