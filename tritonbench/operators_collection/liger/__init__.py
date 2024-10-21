a4e83489b63fd14d5afe99feccf33e0b3af66676
FindHao <yhao@meta.com>
2024-10-21 11:49:24 -0700

    add kl_div, swiglu

--- tritonbench/operators_collection/liger/__init__.py
+++ tritonbench/operators_collection/liger/__init__.py
@@ -1,4 +1,11 @@
-liger_operators = ["FusedLinearCrossEntropy"]
+liger_operators = [
+    # @TODO: fix the names after previous PR is merged
+    "FusedLinearCrossEntropy",
+    "cross_entropy",
+    "geglu",
+    "kl_div",
+    "swiglu",
+]
 
 
 def get_operators():
