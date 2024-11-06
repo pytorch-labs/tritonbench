35a70dd8088d5b35520168d7313317ee9a27ec99
Xu Zhao <xzhao9@meta.com>
2024-11-06 10:17:25 -0500

    Setup root dir

--- benchmarks/nightly/run.py
+++ benchmarks/nightly/run.py
@@ -1,7 +1,26 @@
 """
 Tritonbench nightly run
 """
-from tritonbench.utils.parser import run_in_task
+import os
+import sys
+from os.path import abspath, exists
+
+
+def setup_tritonbench_cwd():
+    original_dir = abspath(os.getcwd())
+
+    for tritonbench_dir in (
+        ".",
+        "../../../tritonbench",
+    ):
+        if exists(tritonbench_dir):
+            break
+
+    if exists(tritonbench_dir):
+        tritonbench_dir = abspath(tritonbench_dir)
+        os.chdir(tritonbench_dir)
+        sys.path.append(tritonbench_dir)
+    return original_dir
 
 
 OPERATORS = [
@@ -13,6 +32,8 @@ OPERATORS = [
 
 
 def run():
+    setup_tritonbench_cwd()
+    from tritonbench.utils.parser import run_in_task
     for op in OPERATORS:
         run_in_task(op)
 
