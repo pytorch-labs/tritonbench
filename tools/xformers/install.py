abb7ac68c5f62c03b69032456c5daf103eb664c1
Xu Zhao <xzhao9@meta.com>
2024-11-21 11:21:19 -0800

    Patch when installing the xformers (#61)
    Summary:
    This is to patch xformers as its FA3 extension build will fail due to lack of linking to libcuda.so: https://github.com/facebookresearch/xformers/pull/1157
    
    Fixes https://github.com/pytorch-labs/tritonbench/issues/20
    
    Pull Request resolved: https://github.com/pytorch-labs/tritonbench/pull/61
    
    Reviewed By: FindHao
    
    Differential Revision: D66273474
    
    Pulled By: xuzhao9
    
    fbshipit-source-id: 81898ccd005750937ac3cfd639c2303975ef1abe

---
+++ tools/xformers/install.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+import sys
+from pathlib import Path
+
+REPO_PATH = Path(os.path.abspath(__file__)).parent.parent.parent
+PATCH_DIR = str(REPO_PATH.joinpath("submodules", "xformers").absolute())
+PATCH_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "xformers.patch")
+
+
+def patch_xformers():
+    try:
+        subprocess.check_output(
+            [
+                "patch",
+                "-p1",
+                "--forward",
+                "-i",
+                PATCH_FILE,
+                "-r",
+                "/tmp/rej",
+            ],
+            cwd=PATCH_DIR,
+        )
+    except subprocess.SubprocessError as e:
+        output_str = str(e.output)
+        if "previously applied" in output_str:
+            return
+        else:
+            print(str(output_str))
+            sys.exit(1)
+
+
+def install_xformers():
+    patch_xformers()
+    os_env = os.environ.copy()
+    os_env["TORCH_CUDA_ARCH_LIST"] = "8.0;9.0;9.0a"
+    XFORMERS_PATH = REPO_PATH.joinpath("submodules", "xformers")
+    cmd = ["pip", "install", "-e", XFORMERS_PATH]
+    subprocess.check_call(cmd, env=os_env)
