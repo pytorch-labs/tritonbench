587f386fbda3b5686e8541ab96049f14fe8f3189
Xu Zhao <xzhao9@meta.com>
2024-11-20 19:40:10 -0500

    Fix the docker build

--- .ci/tritonbench/test-gpu.sh
+++ .ci/tritonbench/test-gpu.sh
@@ -8,11 +8,4 @@ fi
 
 . "${SETUP_SCRIPT}"
 
-# FIXME: patch and install hstu
-sudo apt-get install  -y patch
-python install.py --hstu
-
-# FIXME: install colfax
-python install.py --colfax
-
 python -m unittest test.test_gpu.main -v
