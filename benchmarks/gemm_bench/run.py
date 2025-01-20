9808a9f6eb581f32ad5021cf8d03954b78640f9c
Xu Zhao <xzhao9@meta.com>
2025-01-20 12:51:51 -0500

    Add run

--- benchmarks/gemm_bench/run.py
+++ benchmarks/gemm_bench/run.py
@@ -7,12 +7,29 @@ K = 4096
 
 Print tflops metrics
 """
+import os
+import sys
+from os.path import abspath, exists
 
-import tritonbench
-from tritonbench.utils.parser import get_parser
+def setup_tritonbench_cwd():
+    original_dir = abspath(os.getcwd())
+
+    for tritonbench_dir in (
+        ".",
+        "../../../tritonbench",
+    ):
+        if exists(tritonbench_dir):
+            break
+
+    if exists(tritonbench_dir):
+        tritonbench_dir = abspath(tritonbench_dir)
+        os.chdir(tritonbench_dir)
+        sys.path.append(tritonbench_dir)
+    return original_dir
 
 
 def run():
+    setup_tritonbench_cwd()
     args = [
         "--m",
         "4096",
@@ -27,6 +44,8 @@ def run():
         "--metrics",
         "tflops",
     ]
+    import tritonbench
+    from tritonbench.utils.parser import get_parser
     gemm_op = tritonbench.load_opbench_by_name("gemm")
     parser = get_parser()
     args, extra_args = parser.parse_known_args(args)
