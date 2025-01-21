a6f1bdb0cea211a46269bd8f628fb202ff736847
Xu Zhao <xzhao9@meta.com>
2025-01-21 13:37:55 -0500

    Add parse datetime

--- tritonbench/utils/git_utils.py
+++ tritonbench/utils/git_utils.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from datetime import datetime
 from tritonbench.utils.path_utils import REPO_PATH
 
 
@@ -24,13 +25,17 @@ def get_commit_time(repo, commit) -> str:
     If a commit does not exist, return "unknown" 
     """
     assert os.path.exists(repo), f"{repo} path does not exist."
+    git_date_cmd = ["git", "show", "--no-patch", "--format=%ci", commit]
+    git_date = subprocess.check_output(git_date_cmd, cwd=repo).decode().strip()
+    if not git_date:
+        return "unknown"
+    date_format = "%Y-%m-%d %H:%M:%S %z"
+    parsed_date = datetime.strptime(git_date, date_format)
+    return datetime.strftime(parsed_date, "%Y%m%d%H%M%S")
 
 def get_current_hash(repo: str) -> str:
     """Get the HEAD hash of a git repo.
     repo: local git repo path"""
-    try:
-        cmd = ["git", "rev-parse", "--verify", "HEAD"]
-        output = subprocess.check_output(cmd, cwd=REPO_PATH).decode().strip()
-        return output
-    except subprocess.SubprocessError:
-        return "unknown"
+    cmd = ["git", "rev-parse", "--verify", "HEAD"]
+    output = subprocess.check_output(cmd, cwd=repo).decode().strip()
+    return output
