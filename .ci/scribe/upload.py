24adeb4a4809d0f9added9784a5d82fd5a536364
Xu Zhao <xzhao9@meta.com>
2025-01-21 19:45:54 -0500

    Add script to upload to scribe

--- .ci/scribe/upload.py
+++ .ci/scribe/upload.py
@@ -3,14 +3,114 @@ Upload result json file to scribe.
 """
 
 import argparse
+import json
+import requests
+import time
+
+from collections import defaultdict
 
 CATEGORY_NAME = "perfpipe_pytorch_user_benchmarks"
-
+BENCHMARK_SCHEMA = {
+    "int": ["time"],
+    "normal": [
+        "benchmark_date",
+        "unix_user",
+        "submission_group_id",
+        "cuda_version",
+        "device",
+        "conda_env",
+        "pytorch_commit",
+        "triton_commit",
+        "tritonbench_commit",
+        "triton_branch",
+        "triton_commit_time",
+        "pytorch_commit_time",
+        "tritonbench_commit_time",
+        "github_action",
+        "github_actor",
+        "github_base_ref",
+        "github_ref",
+        "github_ref_protected",
+        "github_repository",
+        "github_run_attempt",
+        "github_run_id",
+        "github_run_number",
+        "github_workflow_ref",
+        "github_workflow_sha",
+        "job_name",
+        "runner_arch",
+        "runner_name",
+        "runner_os",
+    ],
+    "float": ["metric_value"],
+}
 
 class ScribeUploader:
     def __init__(self, category):
         self.category = category
 
+    def _format_message(self, field_dict):
+        assert "time" in field_dict, "Missing required Scribe field 'time'"
+        message = defaultdict(dict)
+        for field, value in field_dict.items():
+            if value is None:
+                continue
+            if field in self.schema["normal"]:
+                message["normal"][field] = str(value)
+            elif field in self.schema["int"]:
+                message["int"][field] = int(value)
+            elif field in self.schema["float"]:
+                message["float"][field] = float(value)
+            else:
+                raise ValueError(
+                    "Field {} is not currently used, "
+                    "be intentional about adding new fields to schema".format(field)
+                )
+        return message
+
+    def _upload(self, messages: list):
+        access_token = os.environ.get(
+            "TORCHBENCH_USERBENCHMARK_SCRIBE_GRAPHQL_ACCESS_TOKEN"
+        )
+        if not access_token:
+            raise ValueError("Can't find access token from environment variable")
+        url = "https://graph.facebook.com/scribe_logs"
+        r = requests.post(
+            url,
+            data={
+                "access_token": access_token,
+                "logs": json.dumps(
+                    [
+                        {
+                            "category": self.category,
+                            "message": json.dumps(message),
+                            "line_escape": False,
+                        }
+                        for message in messages
+                    ]
+                ),
+            },
+        )
+        print(r.text)
+        r.raise_for_status()
+
+    def post_benchmark_results(self, bm_data):
+        messages = []
+        base_message = {
+            "time": int(time.time()),
+        }
+        base_message.update(bm_data["env"])
+        base_message.update(bm_data["github"])
+        base_message["submission_group_id"] = f"tritonbench.{bm_data["name"]}"
+        base_message["unix_user"] = "tritonbench_ci"
+        del base_message["name"]
+        for metric in bm_data["metrics"]:
+            msg = base_message.copy()
+            msg["metric_id"] = metric
+            msg["metric_value"] = bm_data["metrics"][metric]
+            formatted_msg = self._format_message(msg)
+            messages.append(formatted_msg)
+        self._upload(messages)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -18,3 +118,6 @@ if __name__ == "__main__":
         "--json", required=True, type=argparse.FileType("r"), help="Userbenchmark json"
     )
     args = parser.parse_args()
+    uploader = ScribeUploader(category=CATEGORY_NAME)
+    benchmark_data = json.load(args.json)
+    uploader.post_benchmark_results(benchmark_data)
