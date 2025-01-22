ee7635ea084b6b3e6b118d22e4c406fa0601f0a5
Xu Zhao <xzhao9@meta.com>
2025-01-21 22:22:54 -0500

    Run the benchmark in ci mode

---
+++ .ci/tritonbench/run-benchmark.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+
+if [ -z "${SETUP_SCRIPT}" ]; then
+  echo "ERROR: SETUP_SCRIPT is not set"
+  exit 1
+fi
+
+if [ -z "$1" ]; then
+  echo "ERROR: BENCHMARK_NAME must be set as the first argument."
+  exit 1
+fi
+
+. "${SETUP_SCRIPT}"
+
+BENCHMARK_NAME=$1
+
+python "benchmarks/${BENCHMARK_NAME}/run.py" --ci
