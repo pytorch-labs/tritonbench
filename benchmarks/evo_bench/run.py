695283bcc4175470fdaa41a20f6bb252e1a74a26
Xu Zhao <xzhao9@meta.com>
2025-01-30 21:18:27 -0500

    Bugfix

--- benchmarks/evo_bench/run.py
+++ benchmarks/evo_bench/run.py
@@ -2,6 +2,25 @@
 Run benchmark and print tflops metrics
 """
 import argparse
+import os
+import sys
+from os.path import abspath, exists
+
+def setup_tritonbench_cwd():
+    original_dir = abspath(os.getcwd())
+
+    for tritonbench_dir in (
+        ".",
+        "../../../tritonbench",
+    ):
+        if exists(tritonbench_dir):
+            break
+
+    if exists(tritonbench_dir):
+        tritonbench_dir = abspath(tritonbench_dir)
+        os.chdir(tritonbench_dir)
+        sys.path.append(tritonbench_dir)
+    return original_dir
 
 evo_bench_config = {
     "flash_attention_fwd": [
@@ -41,6 +60,7 @@ evo_bench_config = {
 }
 
 def run():
+    setup_tritonbench_cwd()
     from tritonbench.utils.run_utils import run_in_task
     parser = argparse.ArgumentParser()
     parser.add_argument("--op", required=False)
