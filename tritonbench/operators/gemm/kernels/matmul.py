b8c9e71ed5ca0efa9b467b8c8b0dc650a2288ab3
Paul Zhang <paulzhan@fb.com>
2025-02-12 08:05:03 -0800

    Add attribute for triton matmul kernel to use buffer ops for amd

--- tritonbench/operators/gemm/kernels/matmul.py
+++ tritonbench/operators/gemm/kernels/matmul.py
@@ -210,6 +210,7 @@ def _kernel(
     SPLIT_K: tl.constexpr,
     EVEN_K: tl.constexpr,
     AB_DTYPE: tl.constexpr,  #
+    use_buffer_ops: tl.constexpr,  #
 ):
     # matrix multiplication
     pid = tl.program_id(0)
@@ -217,6 +218,14 @@ def _kernel(
     grid_m = tl.cdiv(M, BLOCK_M)
     grid_n = tl.cdiv(N, BLOCK_N)
     # re-order program ID for better L2 performance
+    if use_buffer_ops:
+        tl.assume(stride_am > 0)
+        tl.assume(stride_ak > 0)
+        tl.assume(stride_bk > 0)
+        tl.assume(stride_bn > 0)
+        tl.assume(stride_cm > 0)
+        tl.assume(stride_cn > 0)
+
     width = GROUP_M * grid_n
     group_id = pid // width
     group_size = min(grid_m - group_id * GROUP_M, GROUP_M)
@@ -269,6 +278,8 @@ class _matmul(torch.autograd.Function):
     kernel = _kernel
 
     _locks = {}
+
+    use_buffer_ops = False
 
     @staticmethod
     def _call(a, b, acc_dtype, input_precision, fp8_fast_accum, output_dtype):
@@ -347,6 +358,7 @@ class _matmul(torch.autograd.Function):
             fp8_fast_accum=fp8_fast_accum,  #
             GROUP_M=8,
             AB_DTYPE=ab_dtype,
+            use_buffer_ops=_matmul.use_buffer_ops,
         )
         return c
 
