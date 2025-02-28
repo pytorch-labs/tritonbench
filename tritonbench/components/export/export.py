1f02e8bbc7ae01deff2be3bd7b631c137c14d753
Xu Zhao <xzhao9@meta.com>
2025-02-28 10:36:37 -0500

    fix add export

--- tritonbench/components/export/export.py
+++ tritonbench/components/export/export.py
@@ -3,10 +3,16 @@ Serialize pickled tensors to directory.
 """
 from pathlib import Path
 import pickle
+from tritonbench.utils.input import input_cast
 
 from typing import Callable, Any
 
-def export_data(x_val: str, input: Any, fn_mode: str, fn: Callable, export_type: str, export_dir: str):
+def get_input_gradients(inputs):
+    all_inputs = []
+    input_cast(lambda x: True, lambda y: all_inputs.append(y), inputs)
+    return [x.grad for x in all_inputs]
+
+def export_data(x_val: str, inputs: Any, fn_mode: str, fn: Callable, export_type: str, export_dir: str):
     # pickle naming convention
     # x_<x_val>-input.pkl
     # x_<x_val>-<fn_name>-fwd-output.pkl
@@ -18,10 +24,14 @@ def export_data(x_val: str, input: Any, fn_mode: str, fn: Callable, export_type:
         input_file_name = f"x_{x_val}-input.pkl"
         input_file_path = export_path.joinpath(input_file_name)
         with open(input_file_path, "wb") as ifp:
-            pickle.dump(input, ifp)
+            pickle.dump(inputs, ifp)
     if export_type == "output" or export_type == "both":
         output_file_name = f"x_{x_val}-{fn._name}-{fn_mode}-output.pkl"
         output_file_path = export_path.joinpath(output_file_name)
-        output = fn()
+        if fn_mode == "fwd":
+            output = fn()
+        elif fn_mode == "bwd":
+            # output of the backward pass are the input gradients
+            output = get_input_gradients(inputs)
         with open(output_file_path, "wb") as ofp:
             pickle.dump(output, ofp)
--- tritonbench/operators/flash_attention/operator.py
+++ tritonbench/operators/flash_attention/operator.py
@@ -495,16 +495,13 @@ class Operator(BenchmarkOperator):
         return flops
 
     def get_bwd_fn(self, fwd_fn: Callable) -> Callable:
-        def _bwd(out, dout):
-            out.backward(dout, retain_graph=True)
-            return out.grad()
         o = fwd_fn()
         o_tensor = input_filter(
             lambda x: isinstance(x, torch.Tensor),
             o,
         )
         do = torch.rand_like(o_tensor)
-        fn = lambda: _bwd(o, do)
+        fn = lambda: o.backward(do, retain_graph=True)
         return fn
 
     def get_input_iter(self) -> Generator:
