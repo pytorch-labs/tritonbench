c034f8e2dc375b5014eefcc20e62b9bcefd34cd6
Xu Zhao <xzhao9@meta.com>
2025-02-28 10:14:50 -0500

    fix add export

--- tritonbench/components/export/export.py
+++ tritonbench/components/export/export.py
@@ -17,11 +17,11 @@ def export_data(x_val: str, input: Any, fn_mode: str, fn: Callable, export_type:
     if export_type == "input" or export_type =="both":
         input_file_name = f"x_{x_val}-input.pkl"
         input_file_path = export_path.joinpath(input_file_name)
-        with open(input_file_path, "w") as ifp:
+        with open(input_file_path, "wb") as ifp:
             pickle.dump(input, ifp)
     if export_type == "output" or export_type == "both":
         output_file_name = f"x_{x_val}-{fn._name}-{fn_mode}-output.pkl"
         output_file_path = export_path.joinpath(output_file_name)
         output = fn()
-        with open(output_file_path, "w") as ofp:
+        with open(output_file_path, "wb") as ofp:
             pickle.dump(output, ofp)
