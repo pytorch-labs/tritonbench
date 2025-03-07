79c5302564eebfd92627e6971cddd1cefe6aaf08
Xu Zhao <xzhao9@meta.com>
2025-03-07 12:10:42 -0800

    Upgrade fa3 on B200 (#171)
    Summary:
    To run flash_attention on B200, we need to upgrade the submodule.
    
    flash_attn_v3 does not support B200, but flash_attn_v2 support is done on B200.
    
    Pull Request resolved: https://github.com/pytorch-labs/tritonbench/pull/171
    
    Test Plan:
    ```
    python run.py --op flash_attention --metrics tflops --only flash_v2 --batch 8 --n-heads 16 --d-head 128
    
      (Batch, Heads, SeqLen, Dhead)    flash_v2-tflops
    -------------------------------  -----------------
                  (8, 16, 128, 128)            60.5676
                  (8, 16, 256, 128)           147.492
                  (8, 16, 512, 128)           245.259
                 (8, 16, 1024, 128)           315.111
                 (8, 16, 2048, 128)           352.104
                 (8, 16, 4096, 128)           353.912
                 (8, 16, 8192, 128)           361.591
                (8, 16, 16384, 128)           330.587
                            average           270.828
    ```
    
    Reviewed By: adamomainz
    
    Differential Revision: D70799699
    
    Pulled By: xuzhao9
    
    fbshipit-source-id: 856e572c7dfab354a38b01e787db701c3aa8f81f

--- tools/flash_attn/install.py
+++ tools/flash_attn/install.py
@@ -8,37 +8,7 @@ REPO_PATH = Path(os.path.abspath(__file__)).parent.parent.parent
 CUR_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 
 
-def patch_fa3():
-    patches = ["hopper.patch"]
-    for patch_file in patches:
-        patch_file_path = os.path.join(CUR_DIR, patch_file)
-        submodule_path = str(
-            REPO_PATH.joinpath("submodules", "flash-attention").absolute()
-        )
-        try:
-            subprocess.check_output(
-                [
-                    "patch",
-                    "-p1",
-                    "--forward",
-                    "-i",
-                    patch_file_path,
-                    "-r",
-                    "/tmp/rej",
-                ],
-                cwd=submodule_path,
-            )
-        except subprocess.SubprocessError as e:
-            output_str = str(e.output)
-            if "previously applied" in output_str:
-                return
-            else:
-                print(str(output_str))
-                sys.exit(1)
-
-
 def install_fa3():
-    patch_fa3()
     FA3_PATH = REPO_PATH.joinpath("submodules", "flash-attention", "hopper")
     env = os.environ.copy()
     # limit nvcc memory usage on the CI machine
