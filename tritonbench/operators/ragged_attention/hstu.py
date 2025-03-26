9d12f24974738a7d5b5b32b1605f0bbc2dba0d7d
Xu Zhao <xzhao9@fb.com>
2025-03-25 18:57:21 -0700

    update hstu

--- tritonbench/operators/ragged_attention/hstu.py
+++ tritonbench/operators/ragged_attention/hstu.py
@@ -1,160 +1,90 @@
 import torch
-import triton
 
 from tritonbench.utils.path_utils import add_path, SUBMODULE_PATH
 from tritonbench.utils.triton_op import IS_FBCODE
 
 if IS_FBCODE:
     # Internal Import
-    from hammer.ops.triton.triton_hstu_attention import triton_hstu_mha
+    from generative_recommenders.common import apply_sampling, generate_sparse_seq_len
+    from generative_recommenders.ops.triton.triton_hstu_attention import triton_hstu_mha
 else:
     # OSS Import
     with add_path(str(SUBMODULE_PATH.joinpath("generative-recommenders"))):
-        from generative_recommenders.ops.triton import triton_hstu_mha
+        from generative_recommenders.common import (
+            apply_sampling,
+            generate_sparse_seq_len,
+        )
+        from generative_recommenders.ops.triton.triton_hstu_attention import (
+            triton_hstu_mha,
+        )
 
 from typing import Tuple
 
-class RaggedHSTUAttn(torch.nn.Module):
-    def __init__(
-        self,
-        batch_size,
-        num_heads,
-        num_buckets,
-        sparsity,
-        target_size,
-        sort_by_length,
-        max_seq_len,
-        causal,
-    ) -> None:
-        super().__init__()
-        self.batch_size = batch_size
-        self.num_heads = num_heads
-        self.num_buckets = num_buckets
-        self.sparsity = sparsity
-        self.target_size = target_size
-        self.sort_by_length = sort_by_length
-        self.max_seq_len = max_seq_len
-        self.causal = causal
+triton_hstu_mha = triton_hstu_mha
 
-    def forward(
-        self,
-        q: torch.Tensor,
-        k: torch.Tensor,
-        v: torch.Tensor,
-        seq_offsets: torch.Tensor,
-        num_targets: torch.Tensor,
-    ) -> torch.Tensor:
-
-        out = torch.zeros_like(v)
-        # hardcode the parameters
-        alpha = 0.08838834764831843
-        contextual_seq_len = 0
-        max_attn_len = None
-
-        kwargs = {
-            "N": self.max_seq_len,
-            "alpha": alpha,
-            "q": q,
-            "k": k,
-            "v": v,
-            "seq_offsets": seq_offsets,
-            "causal": self.causal,
-            "num_targets": num_targets,
-            "max_attn_len": max_attn_len,
-            "contextual_seq_len": contextual_seq_len,
-            "sort_by_length": self.sort_by_length,
-        }
-
-        out = triton_hstu_mha.apply(**kwargs)
-
-        return out
-
-
-def generate_sparse_seq_len(
-    size: int,
-    max_seq_len: int,
-    sparsity: float,
-    device: torch.device,
-) -> torch.Tensor:
-    if sparsity == 0.0:
-        return torch.zeros(size=(size,), device=device, dtype=torch.int)
-    elif sparsity == 1.0:
-        return torch.ones(size=(size,), device=device, dtype=torch.int) * max_seq_len
-    elif sparsity >= 0.5:
-        min_seq_len: int = int((2 * sparsity - 1.0) * max_seq_len)
-        return torch.randint(
-            low=min_seq_len,
-            high=max_seq_len,
-            size=(size,),
-            device=device,
-            dtype=torch.int,
-        )
-    else:
-        min_seq_len: int = 0
-        max_seq_len: int = int(2 * sparsity * max_seq_len)
-        return torch.randint(
-            low=min_seq_len,
-            high=max_seq_len,
-            size=(size,),
-            device=device,
-            dtype=torch.int,
-        )
-
-
-try:
-    from hammer.benchmark.module_factory.hstu_utils import (
-        apply_SL,
-    )
-except ImportError:
-    def apply_SL(lengths: torch.Tensor, alpha: float, max_seq_len: int):
-        return lengths
 
 def get_test_inputs(
     batch_size,
-    num_heads,
+    heads,
+    seq_len,
     attn_dim,
     hidden_dim,
-    max_seq_len,
-    sparsity,
+    seq_sparsity,
+    has_delta_q,
+    delta_size,
     target_size,
+    max_attn_len,
+    dtype,
     requires_grad,
-) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, int]:
     lengths = generate_sparse_seq_len(
         size=batch_size,
-        max_seq_len=max_seq_len,
-        sparsity=sparsity,
+        max_seq_len=seq_len,
+        sparsity=seq_sparsity,
         device=torch.device("cuda"),
     )
-    lengths = apply_SL(lengths, alpha=2.0, max_seq_len=max_seq_len)
-    # assume has_delta_q is False
-    num_targets = None
-    if target_size != 0:
-        num_targets = torch.randint(
-            1,
-            target_size + 1,
-            (batch_size,),
-            device=lengths.device,
-            dtype=lengths.dtype,
-        )
-        num_targets = torch.where(num_targets > lengths, lengths, num_targets)
+    lengths = apply_sampling(lengths, alpha=2.0, max_seq_len=seq_len)
+    if has_delta_q:
+        lengths = lengths + delta_size
+        num_targets = torch.ones_like(lengths) * delta_size
+        seq_len = seq_len + delta_size
+    else:
+        delta_size = 0
+        num_targets = None
+        if target_size != 0:
+            num_targets = torch.randint(
+                target_size,
+                target_size + 1,
+                (batch_size,),
+                device=lengths.device,
+                dtype=lengths.dtype,
+            )
+            num_targets = torch.where(num_targets > lengths, lengths, num_targets).to(
+                torch.int32
+            )
+    max_attn_len = max_attn_len if max_attn_len < seq_len else seq_len
     seq_offsets = torch.zeros(
-        (batch_size + 1,),
-        dtype=torch.int64,
-        device="cuda",
+        (batch_size + 1,), dtype=torch.int64, device=torch.device("cuda")
     )
-    seq_offsets[1:] = torch.cumsum(
-        lengths,
-        dim=0,
-    )
+    seq_offsets[1:] = torch.cumsum(lengths, dim=0)
     L = int(seq_offsets[-1].item())
-
-    qkv = torch.randn(
-        (L, num_heads, attn_dim * 2 + hidden_dim),
-        dtype=torch.bfloat16,
-        device="cuda",
-    )
-    q, k, v = torch.split(qkv, [attn_dim, attn_dim, hidden_dim], dim=-1)
-    q.requires_grad_(requires_grad)
-    k.requires_grad_(requires_grad)
-    v.requires_grad_(requires_grad)
-    return q, k, v, seq_offsets, num_targets, max_seq_len
+    x = torch.empty(
+        (L, heads, attn_dim * 2 + hidden_dim),
+        dtype=dtype,
+        device=torch.device("cuda"),
+    ).uniform_(-0.01, 0.01)
+    q, k, v = torch.split(x, [attn_dim, attn_dim, hidden_dim], dim=-1)
+    delta_q = torch.empty(
+        (batch_size * delta_size, heads, attn_dim),
+        dtype=dtype,
+        device=torch.device("cuda"),
+    ).uniform_(-0.1, 0.1)
+    delta_x_offsets = torch.arange(0, delta_size, device=torch.device("cuda"))
+    delta_x_offsets = (seq_offsets[1:] - delta_size).view(
+        batch_size, 1
+    ) + delta_x_offsets.view(1, delta_size)
+    delta_x_offsets = delta_x_offsets.view(-1)
+    q = q.requires_grad_(requires_grad)
+    k = k.requires_grad_(requires_grad)
+    v = v.requires_grad_(requires_grad)
+    return q, k, v, seq_offsets, num_targets, seq_len
--- tritonbench/operators/ragged_attention/operator.py
+++ tritonbench/operators/ragged_attention/operator.py
@@ -14,22 +14,27 @@ from tritonbench.utils.triton_op import (
     register_metric,
 )
 
-from .hstu import get_test_inputs, RaggedHSTUAttn
+from .hstu import get_test_inputs, triton_hstu_mha
 
 
 def parse_op_args(args: List[str]):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch-size", type=int, default=256, help="Batch size")
+    parser.add_argument("--batch-size", type=int, default=512, help="Batch size")
     parser.add_argument("--heads", type=int, default=4, help="Number of heads")
     parser.add_argument("--attn-dim", type=int, default=128)
     parser.add_argument("--hidden-dim", type=int, default=128)
-    parser.add_argument("--causal", action="store_true")
     parser.add_argument("--min-seq-len-log2", type=int, default=8)
-    parser.add_argument("--max-seq-len-log2", type=int, default=15)
-    parser.add_argument("--num-buckets", type=int, default=2048)
-    parser.add_argument("--seq-sparsity", type=float, default=0.95)
+    parser.add_argument("--max-seq-len-log2", type=int, default=16)
+    parser.add_argument("--seq-sparsity", type=float, default=1.0)
+    parser.add_argument("--has-delta-q", type=bool, default=False)
+    parser.add_argument("--delta-size", type=int, default=256)
     parser.add_argument("--target-size", type=int, default=20)
-    parser.add_argument("--sort-by-length", type=bool, default=True)
+    parser.add_argument("--max-attn-len", type=int, default=256)
+    # set to 0 to use hstu_mha
+    parser.add_argument("--min-full-attn-seq-len", type=int, default=0)
+    parser.add_argument("--contextual-seq-len", type=int, default=0)
+    parser.add_argument("--sampling-alpha", type=float, default=1.7)
+    parser.add_argument("--causal", action="store_true")
     return parser.parse_args(args)
 
 
@@ -47,28 +52,35 @@ class Operator(BenchmarkOperator):
         self.hidden_dim = args.hidden_dim
         self.min_seq_len_log2 = args.min_seq_len_log2
         self.max_seq_len_log2 = args.max_seq_len_log2
-        self.num_buckets = args.num_buckets
         self.sparsity = args.seq_sparsity
+        self.has_delta_q = args.has_delta_q
+        self.delta_size = args.delta_size
         self.target_size = args.target_size
-        self.sort_by_length = args.sort_by_length
+        self.max_attn_len = args.max_attn_len
+        self.min_full_attn_seq_len = args.min_full_attn_seq_len
+        self.contextual_seq_len = args.contextual_seq_len
+        self.sampling_alpha = args.sampling_alpha
         self.causal = args.causal
+        self.alpha = 1.0 / self.attn_dim
         self.requires_grad = not (self.mode == Mode.FWD_NO_GRAD)
 
     @register_benchmark()
-    def hstu(
-        self, q, k, v, seq_offsets, num_targets, max_seq_len
-    ):
-        attn = RaggedHSTUAttn(
-            self.batch_size,
-            self.num_heads,
-            self.num_buckets,
+    def hstu(self, q, k, v, seq_offsets, num_targets, max_seq_len):
+        return lambda: triton_hstu_mha(
             max_seq_len,
-            self.sparsity,
-            self.target_size,
-            self.sort_by_length,
-            self.causal,
+            alpha=self.alpha,
+            q=q,
+            k=k,
+            v=v,
+            seq_offsets=seq_offsets,
+            causal=self.causal,
+            dropout_pr=0.0,
+            training=True,
+            num_targets=num_targets,
+            max_attn_len=self.max_attn_len,
+            contextual_seq_len=self.contextual_seq_len,
+            sort_by_length=True,
         )
-        return lambda: attn(q, k, v, seq_offsets, num_targets)
 
     def get_x_val(self, example_inputs):
         seq_len = example_inputs[-1]
@@ -76,10 +88,11 @@ class Operator(BenchmarkOperator):
             self.batch_size,
             self.num_heads,
             seq_len,
-            self.num_buckets,
+            self.attn_dim,
+            self.hidden_dim,
             self.sparsity,
             self.target_size,
-            self.sort_by_length,
+            self.max_attn_len,
         )
 
     def get_input_iter(self):
@@ -89,11 +102,15 @@ class Operator(BenchmarkOperator):
             yield get_test_inputs(
                 self.batch_size,
                 self.num_heads,
+                seq_len,
                 self.attn_dim,
                 self.hidden_dim,
-                seq_len,
                 self.sparsity,
+                self.has_delta_q,
+                self.delta_size,
                 self.target_size,
+                self.max_attn_len,
+                self.dtype,
                 requires_grad=self.requires_grad,
             )
 
@@ -107,33 +124,45 @@ class Operator(BenchmarkOperator):
         fn = lambda: o_tensor.backward(do, retain_graph=True)
         return fn
 
+    def _flops(
+        self,
+        batch_size: int,
+        max_seqlen: int,
+        attn_dim: int,
+        hidden_dim: int,
+        nheads: int,
+        seq_offsets: torch.Tensor,
+        mode: str = "fwd",
+    ) -> float:
+        assert mode in ["fwd", "bwd", "fwd_bwd"]
+        ratio = 2.0  # triangular masking
+        f1 = 0.0
+        f2 = 0.0
+        for i in range(batch_size):
+            seq_len = int((seq_offsets[i + 1] - seq_offsets[i]).item())
+            # (QK^T), dQ = d(QK^T)K, dK^T = Q^Td(QK^T)
+            f1 += 2 * nheads * attn_dim * seq_len**2 // ratio
+            # (QK^T)V, d(QK^T) = dOV^T, dV = (QK^T)^TdO,
+            f2 += 2 * nheads * hidden_dim * seq_len**2 // ratio
+        if mode == "fwd":
+            return f1 + f2  # computes (QK^T) and (QK^T)V
+        elif mode == "bwd":
+            return 3 * f1 + 2 * f2  # computes (QK^T), dQ, dK, dV, d(QK^T)
+        else:
+            return 4 * f1 + 3 * f2
+
     @register_metric()
     def flops(
         self, fn_name, example_inputs, metrics: BenchmarkOperatorMetrics
     ) -> float:
-        ratio = 2.0  # triangular masking
-        f1 = 0.0
-        f2 = 0.0
-        jagged = True
-        q, k, v, seq_offsets, timestamps, num_targets, seq_len = example_inputs
-        _, nheads, attn_dim = q.shape
-        _, _, hidden_dim = v.shape
-        max_seqlen = timestamps.size(1) - 1
-
-        for i in range(self.batch_size):
-            seq_len = (
-                int((seq_offsets[i + 1] - seq_offsets[i]).item())
-                if jagged
-                else max_seqlen
-            )
-            # (QK^T), dQ = d(QK^T)K, dK^T = Q^Td(QK^T)
-            f1 += 2 * self.num_heads * attn_dim * seq_len**2 // ratio
-            # (QK^T)V, d(QK^T) = dOV^T, dV = (QK^T)^TdO,
-            f2 += 2 * self.num_heads * hidden_dim * seq_len**2 // ratio
-        if self.mode == Mode.FWD:
-            flops = f1 + f2  # computes (QK^T) and (QK^T)V
-        elif self.mode == Mode.BWD:
-            flops = 3 * f1 + 2 * f2  # computes (QK^T), dQ, dK, dV, d(QK^T)
-        elif self.mode == Mode.FWD_BWD:
-            flops = 4 * f1 + 3 * f2
+        q, k, v, seq_offsets, num_targets, max_seq_len = example_inputs
+        flops = self._flops(
+            self.batch_size,
+            max_seq_len,
+            self.attn_dim,
+            self.hidden_dim,
+            self.num_heads,
+            seq_offsets,
+            mode=self.mode.value,
+        )
         return flops
