e830c7d4c3d2becff7e05a9ef96026a6e946f9b2
Xu Zhao <xzhao9@fb.com>
2025-04-12 13:04:39 -0700

    fix tk backward

--- tritonbench/operators/flash_attention/tk.py
+++ tritonbench/operators/flash_attention/tk.py
@@ -7,28 +7,19 @@ import torch
 class _TKAttn(torch.autograd.Function):
     @staticmethod
     def forward(ctx, q, k, v, causal) -> torch.Tensor:
-        saved_tensors = [q, k, v]
-        ctx.save_for_backward(*saved_tensors)
         ctx.causal = causal
-        return tk.mha_forward(q, k, v, causal)
+        o, l_vec = tk.mha_forward(q, k, v, causal)
+        saved_tensors = [q, k, v, o, l_vec]
+        ctx.save_for_backward(*saved_tensors)
+        return o
 
     @staticmethod
     def backward(ctx, do) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, None]:
         causal = ctx.causal
-        q, k, v = ctx.saved_tensors
-        o = torch.zeros_like(q).contiguous()
+        q, k, v, o, l_vec = ctx.saved_tensors
         dq = torch.empty_like(q)
         dk = torch.empty_like(k)
         dv = torch.empty_like(v)
-        l_vec = torch.zeros(
-            q.shape[0],
-            q.shape[1],
-            q.shape[2],
-            1,
-            device=q.device,
-            dtype=torch.float,
-            requires_grad=False,
-        ).contiguous()
         dq, dk, dv = tk.mha_backward(q, k, v, o, l_vec, do, causal)
         return dq, dk, dv, None
 
