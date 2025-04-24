43cb5488bcbf8636b85b04f38a74014c6f1cd4ae
Xu Zhao <xzhao9@fb.com>
2025-04-23 17:04:04 -0700

    add metadata

--- benchmarks/nightly/run.py
+++ benchmarks/nightly/run.py
@@ -10,8 +10,6 @@ import os
 import sys
 from os.path import abspath, exists
 
-from ..common import setup_tritonbench_cwd
-
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
@@ -36,35 +34,52 @@ def setup_tritonbench_cwd():
 OPERATOR_BENCHMARKS = {
     # launch latency will not be shown in the dashboard
     "launch_latency": [
-        "--op", "launch_latency",
-        "--skip", "nop_inductor_kernel",
-        "--metrics", "latency,walltime",
+        "--op",
+        "launch_latency",
+        "--skip",
+        "nop_inductor_kernel",
+        "--metrics",
+        "latency,walltime",
     ],
     "softmax": [
-        "--op", "softmax",
-        "--metrics", "speedup",
-        "--num-inputs", "6",
+        "--op",
+        "softmax",
+        "--metrics",
+        "speedup",
+        "--num-inputs",
+        "6",
         "--cudagraph",
     ],
     "bf16_gemm": [
-        "--op", "gemm",
-        "--only", "triton_tutorial_matmul",
-        "--precision", "bf16",
-        "--metrics", "speedup",
-        "--num-inputs", "4",
+        "--op",
+        "gemm",
+        "--only",
+        "triton_tutorial_matmul",
+        "--precision",
+        "bf16",
+        "--metrics",
+        "speedup",
+        "--num-inputs",
+        "4",
         "--cudagraph",
     ],
     "bf16_flash_attention_fwd": [
-        "--op", "flash_attention",
-        "--only", "triton_tutorial_flash_v2_opt,flash_v3"
-        "--baseline", "flash_v3",
-        "--metrics", "tflops,speedup",
+        "--op",
+        "flash_attention",
+        "--only",
+        "triton_tutorial_flash_v2_opt,flash_v3" "--baseline",
+        "flash_v3",
+        "--metrics",
+        "tflops,speedup",
     ],
     "bf16_flash_attention_bwd": [
-        "--op", "flash_attention",
-        "--only", "triton_tutorial_flash_v2_opt,flash_v3"
-        "--baseline", "flash_v3",
-        "--metrics", "tflops,speedup",
+        "--op",
+        "flash_attention",
+        "--only",
+        "triton_tutorial_flash_v2_opt,flash_v3" "--baseline",
+        "flash_v3",
+        "--metrics",
+        "tflops,speedup",
         "--bwd",
     ],
 }
