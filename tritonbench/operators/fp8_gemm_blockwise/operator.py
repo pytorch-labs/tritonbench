aaa293ea5bba564742bb878debb3e9ac6483f875
Dhruva Kaushal <dhruva@meta.com>
2025-04-28 18:05:41 -0700

    removing fp8_gemm_blockwise from running on AMD
    Summary:
    triton_fp8_block uses some nvidia toolchain specific functions to calculate tflops. Removing it until the AMD equivalent is supported.
    
    HAS_CUTLASS --> should run only on nvidia.
    
    This is causing failures in our dashboard experiments.
    
    Reviewed By: njriasan
    
    Differential Revision: D73704745
    
    fbshipit-source-id: 9840a158636e9fb10720a1e445463885a9a8d9a5

--- tritonbench/operators/fp8_gemm_blockwise/operator.py
+++ tritonbench/operators/fp8_gemm_blockwise/operator.py
@@ -7,6 +7,8 @@ import fbgemm_gpu.experimental.gen_ai  # noqa: F401
 import torch
 import triton
 
+from tritonbench.utils.env_utils import is_cuda
+
 from tritonbench.utils.triton_op import (
     BenchmarkOperator,
     BenchmarkOperatorMetrics,
@@ -27,26 +29,32 @@ def parse_args(args: List[str]) -> argparse.Namespace:
     return args
 
 
-try:
-    from fbgemm_gpu.experimental.gemm.triton_gemm.fp8_gemm import (
-        matmul_fp8_block as triton_fp8_block,
-    )
-
-    HAS_TRITON = True
-except:
-    HAS_TRITON = False
-
-
-HAS_CUTLASS = True
-try:
-    cutlass_fp8_block = torch.ops.llama_cpp.fp8_blockwise_matmul
-except:
+HAS_TRITON = False
+if is_cuda():
     try:
-        import fbgemm_gpu.experimental.gen_ai
+        from fbgemm_gpu.experimental.gemm.triton_gemm.fp8_gemm import (
+            matmul_fp8_block as triton_fp8_block,
+        )
 
-        cutlass_fp8_block = torch.ops.fbgemm.f8f8bf16_blockwise
+        HAS_TRITON = True
     except:
-        HAS_CUTLASS = False
+        HAS_TRITON = False
+# The triton_fp8_block is unsupported on non-NVIDIA GPUs.
+# TODO: add support for AMD.
+
+HAS_CUTLASS = False
+if is_cuda():
+    try:
+        cutlass_fp8_block = torch.ops.llama_cpp.fp8_blockwise_matmul
+        HAS_CUTLASS = True
+    except:
+        try:
+            import fbgemm_gpu.experimental.gen_ai
+
+            cutlass_fp8_block = torch.ops.fbgemm.f8f8bf16_blockwise
+            HAS_CUTLASS = True
+        except:
+            HAS_CUTLASS = False
 
 
 BUILDIN_SHAPES = [
