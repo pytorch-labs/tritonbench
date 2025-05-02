pytorch-labs/tritonbench

commit d4a1e60650d332d516bf62aca3aba196271a85d1

Add gemm analyzer and input loaders
Summary:
1. Load gemm/addmm/bmm configs from inductor log and output json and csv to given directory.
2. Load the result json files into input tensors and run with Tritonbench

Reviewed By: PaulZhang12

Differential Revision: D73898451

fbshipit-source-id: 6a89fab13dc9eff8058f7f9a5e6616e0bc61b829

tritonbench/utils/triton_op.py
@@ -752,6 +752,12 @@ class BenchmarkOperator(metaclass=PostInitProcessor):
 
     # Run the post initialization
     def __post__init__(self):
+        if self.tb_args.input_loader:
+            from tritonbench.data.fb.input_loader import get_input_loader
+
+            self.get_input_iter = get_input_loader(
+                self, self.name, self.tb_args.input_loader
+            )
         self._available_num_inputs = self.count_example_inputs()
         if self._num_inputs is None:
             self._num_inputs = self._available_num_inputs - self._input_id

tritonbench/utils/parser.py
@@ -205,6 +205,11 @@ def get_parser(args=None):
     )
 
     if is_fbcode():
+        parser.add_argument(
+            "--input-loader",
+            type=str,
+            help="Load input file from Tritonbench data JSON.",
+        )
         parser.add_argument("--log-scuba", action="store_true", help="Log to scuba.")
         parser.add_argument(
             "--logging-group",

commit 0ee663d3fe814dc9f04748f3a6ae49017816264e

Only load input loader in fbcode (#217)
Summary:
https://github.com/pytorch-labs/tritonbench/commit/d4a1e60650d332d516bf62aca3aba196271a85d1 broke the code for OSS, fixing it.

Pull Request resolved: https://github.com/pytorch-labs/tritonbench/pull/217

Reviewed By: FindHao

Differential Revision: D74087811

Pulled By: xuzhao9

fbshipit-source-id: bc941d586b6211655ae2f28179173450277b8604

tritonbench/utils/triton_op.py
@@ -752,7 +752,7 @@ class BenchmarkOperator(metaclass=PostInitProcessor):
 
     # Run the post initialization
     def __post__init__(self):
-        if self.tb_args.input_loader:
+        if is_fbcode() and self.tb_args.input_loader:
             from tritonbench.data.fb.input_loader import get_input_loader
 
             self.get_input_iter = get_input_loader(