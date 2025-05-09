e83d02d811499e5a6259373debdc1a9f7b866cc9
Xu Zhao <xzhao9@meta.com>
2025-05-09 08:38:25 -0400

    fix shellcheck

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,8 +2,9 @@
 
 # Entrypoint of the docker image
 
+# shellcheck source=/workspace/setup_instance.sh
 . "${SETUP_SCRIPT}"
 
-cd /workspace/tritonbench
+cd /workspace/tritonbench || exit
 
 python run.py
