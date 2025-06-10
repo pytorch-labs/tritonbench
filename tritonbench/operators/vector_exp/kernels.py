94f1b8645256fc04887df67cc2e537e1a1d7e536
Shawn Zhong <shawnzhong@meta.com>
2025-06-10 13:52:03 -0700

    Add vector_exp to TritonBench
    Summary: Implement the kernel `triton_exp_kernel` and compared its performance with `torch.exp`
    
    Reviewed By: robieta
    
    Differential Revision: D76314872
    
    fbshipit-source-id: 973ad84e996c4886086b7b42415e769dfd2eff2f

---
+++ tritonbench/operators/vector_exp/kernels.py
@@ -0,0 +1,29 @@
+import triton
+import triton.language as tl
+
+
+@triton.jit
+def triton_exp_kernel(
+    x_ptr,  # *Pointer* to input vector.
+    output_ptr,  # *Pointer* to output vector.
+    n_elements,  # Size of the vector.
+    BLOCK_SIZE: tl.constexpr,  # Number of elements each program should process.
+    # NOTE: `constexpr` so it can be used as a shape value.
+):
+    # There are multiple 'programs' processing different data. We identify which program
+    # we are here:
+    pid = tl.program_id(axis=0)  # We use a 1D launch grid so axis is 0.
+    # This program will process inputs that are offset from the initial data.
+    # For instance, if you had a vector of length 256 and block_size of 64, the programs
+    # would each access the elements [0:64, 64:128, 128:192, 192:256].
+    # Note that offsets is a list of pointers:
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    # Create a mask to guard memory operations against out-of-bounds accesses.
+    mask = offsets < n_elements
+    # Load x from DRAM, masking out any extra elements in case the input is not a
+    # multiple of the block size.
+    x = tl.load(x_ptr + offsets, mask=mask)
+    output = tl.exp(x)
+    # Write exp(x) back to DRAM.
+    tl.store(output_ptr + offsets, output, mask=mask)
---
+++ tritonbench/operators/vector_exp/operator.py
@@ -0,0 +1,84 @@
+from typing import Generator, List
+
+import torch
+import triton
+
+from tritonbench.utils.triton_op import (
+    BenchmarkOperator,
+    BenchmarkOperatorMetrics,
+    register_benchmark,
+    register_metric,
+)
+
+from .kernels import triton_exp_kernel
+
+
+class Operator(BenchmarkOperator):
+    @register_metric()
+    def gbps(self, fn_name, example_inputs, metrics: BenchmarkOperatorMetrics):
+        return (
+            3
+            * example_inputs[0].element_size()
+            * example_inputs[0].numel()
+            / metrics.latency
+            * 1e-6
+        )
+
+    @register_benchmark()
+    def triton_exp(self, x: torch.Tensor):
+        # We need to preallocate the output.
+        output = torch.empty_like(x)
+        n_elements = output.numel()
+        # The SPMD launch grid denotes the number of kernel instances that run in parallel.
+        # It is analogous to CUDA launch grids. It can be either Tuple[int], or Callable(metaparameters) -> Tuple[int].
+        # In this case, we use a 1D grid where the size is the number of blocks:
+        grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]),)
+        # NOTE:
+        #  - Each torch.tensor object is implicitly converted into a pointer to its first element.
+        #  - `triton.jit`'ed functions can be indexed with a launch grid to obtain a callable GPU kernel.
+        #  - Don't forget to pass meta-parameters as keywords arguments.
+
+        def _inner():
+            return_val = triton_exp_kernel[grid](x, output, n_elements, BLOCK_SIZE=1024)
+            return return_val
+
+        return _inner
+
+    @register_benchmark(baseline=True)
+    def torch_exp(self, x: torch.Tensor):
+        return lambda: torch.exp(x)
+
+    def get_x_vals(self) -> List[int]:
+        return [2**i for i in range(12, 28, 1)]
+
+    def get_x_val(self, example_inputs):
+        return len(example_inputs[0])
+
+    def plot(self):
+        @triton.testing.perf_report(
+            triton.testing.Benchmark(
+                x_names=["size"],  # Argument names to use as an x-axis for the plot.
+                x_vals=self.x_vals,  # Different possible values for `x_name`.
+                x_log=True,  # x axis is logarithmic.
+                line_arg="provider",  # Argument name whose value corresponds to a different line in the plot.
+                line_vals=[
+                    "torch_exp",
+                    "triton_exp",
+                ],  # Possible values for `line_arg`.
+                line_names=["Torch", "Triton"],  # Label name for the lines.
+                styles=[("blue", "-"), ("green", "-")],  # Line styles.
+                ylabel="GB/s",  # Label name for the y-axis.
+                plot_name="vector-exp-performance",  # Name for the plot. Used also as a file name for saving the plot.
+                args={},  # Values for function arguments not in `x_names` and `y_name`.
+            )
+        )
+        def _plot(size, provider):
+            gbps, max_gbps, min_gbps = self.output.get_y_vals(size, provider, "gbps")
+            return gbps, max_gbps, min_gbps
+
+        _plot.run(show_plots=True, print_data=True, save_path="/tmp/vector_exp")
+
+    def get_input_iter(self) -> Generator:
+        for size in self.get_x_vals():
+            x = torch.rand(size, device=self.device, dtype=self.dtype)
+            yield (x,)
