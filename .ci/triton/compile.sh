1160902befc7cb555a948500c35fa5ddcf33b5a2
Xu Zhao <xzhao9@fb.com>
2025-06-18 08:27:59 -0700

    add abtest to triton

---
+++ .ci/triton/compile.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+# Print usage
+usage() {
+    echo "Usage: $0 --repo <repo-path> --commit <commit-hash> --side <a|b|single>"
+    exit 1
+}
+
+# remove triton installations
+remove_triton() {
+    # delete the original triton directory
+    TRITON_PKG_DIR=$(python -c "import triton; import os; print(os.path.dirname(triton.__file__))")
+    # make sure all pytorch triton has been uninstalled
+    pip uninstall -y triton
+    pip uninstall -y triton
+    pip uninstall -y triton
+    rm -rf "${TRITON_PKG_DIR}"
+}
+
+checkout_triton() {
+    REPO=$1
+    COMMIT=$2
+    TRITON_INSTALL_DIR=$3
+    TRITON_INSTALL_DIRNAME=$(basename "${TRITON_INSTALL_DIR}")
+    TRITON_INSTALL_BASEDIR=$(dirname "${TRITON_INSTALL_DIR}")
+    cd "${TRITON_INSTALL_BASEDIR}"
+    git clone "https://github.com/${REPO}.git" "${TRITON_INSTALL_DIRNAME}"
+    cd "${TRITON_INSTALL_DIR}"
+    git checkout "${COMMIT}"
+}
+
+install_triton() {
+    TRITON_INSTALL_DIR=$1
+    cd "${TRITON_INSTALL_DIR}"
+    # install main triton
+    pip install ninja cmake wheel pybind11; # build-time dependencies
+    pip install -r python/requirements.txt
+    pip install -e .
+}
+
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --repo) REPO="$2"; shift ;;
+        --commit) COMMIT="$2"; shift ;;
+        --side) SIDE="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; usage ;;
+    esac
+    shift
+done
+
+# BASE_CONDA_ENV and SETUP_SCRIPT must be set
+if [ -z "${BASE_CONDA_ENV}" ]; then
+  echo "ERROR: BASE_CONDA_ENV is not set"
+  exit 1
+fi
+if [ -z "${SETUP_SCRIPT}" ]; then
+  echo "ERROR: SETUP_SCRIPT is not set"
+  exit 1
+fi
+
+# Validate arguments
+if [ -z "${REPO}" ] || [ -z "${COMMIT}" ] || [ -z "${SIDE}" ]; then
+    echo "Missing required arguments."
+    usage
+fi
+
+# clone BASE_CONDA_ENV
+CONDA_ENV=${BASE_CONDA_ENV} . "${SETUP_SCRIPT}"
+conda activate "${BASE_CONDA_ENV}"
+# Remove the conda env if exists
+conda remove --name "${CONDA_ENV}" -y --all || true
+conda create --name "${CONDA_ENV}" -y --clone "${BASE_CONDA_ENV}"
+conda activate "${CONDA_ENV}"
+
+. "${SETUP_SCRIPT}"
+
+remove_triton
+
+# checkout triton to /workspace/abtest/<env-name> and install it
+if [ "${SIDE}" == "single" ]; then
+    TRITON_INSTALL_DIR=/workspace/triton
+elif [ "${SIDE}" == "a" ]; then
+    TRITON_INSTALL_DIR=/workspace/abtest/triton_a
+elif [ "${SIDE}" == "b" ]; then
+    TRITON_INSTALL_DIR=/workspace/abtest/triton_b
+else
+    echo "Unknown side: ${SIDE}"
+    exit 1
+fi
+
+checkout_triton "${REPO}" "${COMMIT}" "${TRITON_INSTALL_DIR}"
+install_triton "${TRITON_INSTALL_DIR}"
+
+SIDE_UPPER="${SIDE^^}"
+
+# export Triton repo related envs
+# these envs will be used in nightly runs and other benchmarks
+cd "${TRITON_INSTALL_DIR}"
+TRITONBENCH_TRITON_COMMIT=$(git rev-parse --verify HEAD)
+echo "export TRITONBENCH_TRITON_COMMIT=${TRITONBENCH_TRITON_COMMIT}" >> /workspace/setup_instance.sh
+echo "export TRITONBENCH_TRITON_REPO=${TRITON_INSTALL_DIR}" >> /workspace/setup_instance.sh
--- .ci/tritonbench/run-benchmark.sh
+++ .ci/tritonbench/run-benchmark.sh
@@ -11,11 +11,22 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-. "${SETUP_SCRIPT}"
-
 BENCHMARK_NAME=$1
+shift
+
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --conda-env) CONDA_ENV="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; usage ;;
+    esac
+    shift
+done
 
 tritonbench_dir=$(dirname "$(readlink -f "$0")")/../..
 cd "${tritonbench_dir}"
 
+echo "Running ${BENCHMARK_NAME} benchmark under conda env ${CONDA_ENV}"
+
+. "${SETUP_SCRIPT}"
 python "benchmarks/${BENCHMARK_NAME}/run.py" --ci
