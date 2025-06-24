pytorch-labs/tritonbench

commit 55426907df0cbb0abbae43cd3d76c84beed2ec89

Profile `jagged_sum` and compute occupancy
Summary:
The patch profile `jagged_sum` kernel and compute occupancy based on the collected timing and smid results.

- Profile `triton_jagged_sum_kernel_variable_length_loop_sum_then_buffer` and `triton_jagged_sum_kernel_variable_length_loop_buffer_then_sum`. The two kernels are the most performant as reported in https://docs.google.com/presentation/d/1m2mS501DEV6uHmYJQzcMt41I7tw0akwqZkYDmfPVyjg/edit?slide=id.g27836168f6c_0_310#slide=id.g27836168f6c_0_310 (the original author of the kernel). The rest of the kernels are written by the author as part of the development process, and is thus not profiled.
- Compute occupancy using the formula: `gpu_time / (wall_time * active_sm)`. We consider the occupancy for the CUs that actually participated in computation.

```
  x_val    triton_jagged_sum_no_pad_variable_length_loop-occupancy
-------  ---------------------------------------------------------
      4                                                   0.785124
      4                                                   0.760989
      4                                                   0.684797
      4                                                   0.633027
      4                                                   0.651905
      4                                                   0.573223
      4                                                   0.561628
      4                                                   0.534789
      4                                                   0.552898
     64                                                   0.826684
     64                                                   0.791789
     64                                                   0.746358
     64                                                   0.72617
     64                                                   0.681499
     64                                                   0.54256

tritonbench/operators/jagged_sum/kernels.py
@@ -2,6 +2,7 @@ import itertools
 
 import triton
 import triton.language as tl
+from tritonbench.kernels.profile import profile_mem_pre_hook, smid, time
 
 
 BLOCK_SIZES = [2**n for n in range(3, 7, 3)]
@@ -170,6 +171,7 @@ def triton_jagged_sum_kernel_simple_fused_buffer_then_sum(
             },
             num_warps=w,
             num_stages=s,
+            pre_hook=profile_mem_pre_hook,
         )
         for b_r, b_m, w, s in itertools.product(
             BLOCK_SIZES,  # block sizes on non-reduction dimension
@@ -242,6 +244,7 @@ def triton_jagged_sum_kernel_variable_length_loop_sum_then_buffer(
             },
             num_warps=w,
             num_stages=s,
+            pre_hook=profile_mem_pre_hook,
         )
         for b_r, b_m, w, s in itertools.product(
             BLOCK_SIZES,  # block sizes on non-reduction dimension
@@ -262,7 +265,11 @@ def triton_jagged_sum_kernel_variable_length_loop_buffer_then_sum(
     # block sizes (input)
     BLOCK_SIZE_RAGGED: tl.constexpr,  # number of elements in ragged dimension per block, with logical dimensions (B, *, M)
     BLOCK_SIZE_M: tl.constexpr,  # number of elements in M-th dimension per block, with logical dimensions (B, *, M)
+    profile_mem=None,  # *Pointer* to profile_mem
 ):
+    if profile_mem is not None:
+        start = time()
+
     pid = tl.program_id(axis=0)  # i-th tensor in nested tensor
     pid_ragged = pid // tl.cdiv(M, BLOCK_SIZE_M)
     pid_m = pid % tl.cdiv(M, BLOCK_SIZE_M)
@@ -303,3 +310,9 @@ def triton_jagged_sum_kernel_variable_length_loop_buffer_then_sum(
     output_mask = output_offsets < (M * (pid_ragged + 1))
 
     tl.store(output_ptr + output_offsets, buffer_view, mask=output_mask)
+
+    if profile_mem is not None:
+        end = time()
+        tl.store(profile_mem + pid * 3, smid())
+        tl.store(profile_mem + pid * 3 + 1, start)
+        tl.store(profile_mem + pid * 3 + 2, end)

tritonbench/kernels/profile.py
@@ -2,6 +2,9 @@
 Common kernels used for profiling.
 """
 
+from typing import Tuple
+
+import torch
 import triton
 import triton.language as tl
 
@@ -81,3 +84,17 @@ def smid(_semantic=None):
         return tl.extra.cuda.smid()
     else:
         tl.static_assert(False, "Unsupported platform")
+
+
+def profile_mem_pre_hook(nargs):
+    """
+    Pre-hook for allocating memory for profiling.
+    """
+    profile_mem: torch.Tensor | None = nargs.get("profile_mem")
+    if profile_mem is not None:
+        shape: Tuple[int] = nargs["grid"](nargs)
+        # Add another dimension for simd, start, and end time
+        shape = (*shape, 3)
+        # We use `resize_` to avoid creating a new tensor.
+        # Otherwise, the caller cannot reference the tensor.
+        profile_mem.resize_(shape)

tritonbench/operators/jagged_sum/operator.py
@@ -1,8 +1,5 @@
 import argparse
-import itertools
-import math
 import os
-import random
 from typing import Callable, Generator, List, Optional, Tuple
 
 import torch
@@ -67,13 +64,15 @@ def execute_kernel_simple_fused(x, max_seqlen, sum_then_buffer):
             MAX_SEQLEN=max_seqlen,
         )
 
-    return kernel_output
+    return None
 
 
 def execute_kernel_variable_length_loop(x, sum_then_buffer):
     B, M = x.shape[0], x.shape[2]
     grid = lambda meta: ((len(x.offsets()) - 1) * triton.cdiv(M, meta["BLOCK_SIZE_M"]),)
     kernel_output = torch.zeros((B, M), device=x.device)
+    # The size of the profile memory will be determined by the autotuner
+    profile_mem = torch.empty(0, dtype=torch.int64, device=x.device)
 
     if sum_then_buffer:
         triton_jagged_sum_kernel_variable_length_loop_sum_then_buffer[grid](
@@ -81,6 +80,7 @@ def execute_kernel_variable_length_loop(x, sum_then_buffer):
             x.offsets(),
             kernel_output,
             M=M,
+            profile_mem=profile_mem,
         )
     else:
         triton_jagged_sum_kernel_variable_length_loop_buffer_then_sum[grid](
@@ -88,13 +88,14 @@ def execute_kernel_variable_length_loop(x, sum_then_buffer):
             x.offsets(),
             kernel_output,
             M=M,
+            profile_mem=profile_mem,
         )
 
-    return kernel_output
+    return profile_mem
 
 
 class Operator(BenchmarkOperator):
-    DEFAULT_METRICS = ["latency", "accuracy", "best_config"]
+    DEFAULT_METRICS = ["latency", "best_config"]
     DEFAULT_PRECISION = "fp32"
 
     def __init__(
@@ -285,7 +286,8 @@ class Operator(BenchmarkOperator):
             )
         )
         def _plot(x_axis, provider):
-            return self.output.get_y_vals(x_axis, provider, "latency")
+            latency = self.output.get_y_vals(x_axis, provider, "latency")
+            return latency.p50, latency.max, latency.min
 
         save_path = (
             os.getcwd()

tritonbench/utils/triton_op.py
@@ -260,6 +260,8 @@ class BenchmarkOperatorMetrics:
     kernel_source_hash: Optional[str] = None
     # cuda time
     cuda_time: Optional[float] = None
+    # occupancy, computed as the ratio of actual GPU time to maximum possible GPU time
+    occupancy: Optional[float] = None
 
 
 BUILTIN_METRICS = {x.name for x in fields(BenchmarkOperatorMetrics)} - {"extra_metrics"}
@@ -1235,6 +1237,8 @@ class BenchmarkOperator(metaclass=PostInitProcessor):
                 metrics.tflops = self.tflops(fn_name, self.example_inputs, metrics)
             if "gbps" in self.required_metrics:
                 metrics.gbps = self.gbps(fn, self.example_inputs, metrics)
+            if "occupancy" in self.required_metrics:
+                metrics.occupancy = self.occupancy(fn, self.example_inputs, metrics)
             if "compile_time" in self.required_metrics:
                 compile_time, compile_time_by_stage = self.compile_time(
                     input_id, fn_name, metrics
@@ -1836,6 +1840,35 @@ class BenchmarkOperator(metaclass=PostInitProcessor):
             self._op_flops[fn] = _get_flops(self, fn)
         op_flops = self._op_flops[fn]
         return op_flops / metrics.latency / 1e12 * 1e3
+
+    def occupancy(
+        self, fn: Callable, example_inputs: Any, metrics: BenchmarkOperatorMetrics
+    ) -> float:
+        profile_mem = fn()
+        if profile_mem is None:
+            return None
+
+        # each row of profile_mem is (smid, start, end)
+        smids = profile_mem[:, 0]
+        start_times = profile_mem[:, 1]
+        end_times = profile_mem[:, 2]
+
+        # We use the actual number of SMs that are active to calculate occupancy.
+        # max_sm counts the total number of SMs on the device, but not all of them
+        # are active.
+        active_sm = torch.unique(smids).numel()
+        # max_sm = torch.cuda.get_device_properties("cuda").multi_processor_count
+
+        # Wall time measures the actual time taken to run the kernel.
+        # GPU time measures the time spent on the GPU, aggregated across all SMs.
+        wall_time = torch.max(end_times) - torch.min(start_times)
+        gpu_time = torch.sum(end_times - start_times)
+
+        # We define the occupancy to be the ratio of actual GPU time to the maximum
+        # possible GPU time using the active SMs.
+        NUM_WAVES = 2
+        occupancy = gpu_time / (wall_time * active_sm) / NUM_WAVES
+        return occupancy
 
     def dump_ir(self, input_id, fn):
         from unittest import mock

commit b2dba91695bf47797eeb50e2c9a555039b9e3b90

Fix OSS linter error
Summary:
Fix linter error introduced in https://github.com/pytorch-labs/tritonbench/commit/55426907df0cbb0abbae43cd3d76c84beed2ec89.

E.g., https://github.com/pytorch-labs/tritonbench/actions/runs/15859983368/job/44714529297

Differential Revision: D77257991

tritonbench/operators/jagged_sum/kernels.py
@@ -2,6 +2,7 @@ import itertools
 
 import triton
 import triton.language as tl
+
 from tritonbench.kernels.profile import profile_mem_pre_hook, smid, time
 
 