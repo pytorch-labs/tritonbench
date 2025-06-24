542b16a8a51a88f4a86b21472e484c9fdf40daf2
Shawn Zhong <shawnzhong@meta.com>
2025-06-23 20:09:39 -0700

    Implement `smid` for AMD GPUs
    Summary:
    This diff implements smid for AMD GPUs based on the discussion with CRobeck and code from
    https://github.com/triton-lang/triton/blob/4d3c4980044eb40fae31490ee07fcf94b6bb7f4a/third_party/amd/backend/include/hip/amd_detail/amd_device_functions.h#L891-L937
    
    Example output for smids:
    
    ```
    tensor([1004002000, 1005002008, 1006001006, 1007001004, 1000002009, 1001003000,
            1002003002, 1003001008, 1004000006, 1005000000, 1006003002, 1007003005,
            1000000008, 1001002001, 1002002006, 1003003006, 1004001000, 1005001004,
            1006002003, 1007002004, 1000001000, 1001000004, 1002000006, 1003002001,
            1004003004, 1005003003, 1006000006, 1007000001, 1000003008, 1001001004,
            1002001008, 1003000000, 1004002001, 1005002009, 1006001007, 1007001005,
            1000002000, 1001003001, 1002003003, 1003001009, 1004000007, 1005000001,
            1006003003, 1007003006, 1000000000, 1001002002, 1002002007, 1003003007,
            1004001001, 1005001005, 1006002004, 1007002005, 1000001001, 1001000005,
            1002000007, 1003002002, 1004003005, 1005003004, 1006000007, 1007000002,
            1000003000, 1001001005, 1002001009, 1003000001, 1004002002, 1005002000,
            1006001008, 1007001006, 1000002001, 1001003002, 1002003004, 1003001000,
            1004000008, 1005000002, 1006003004, 1007003007, 1000000001, 1001002003,
            1002002008, 1003003008, 1004001002, 1005001006, 1006002005, 1007002006,
            1000001002, 1001000006, 1002000008, 1003002003, 1004003006, 1005003005,
            1006000008, 1007000003, 1000003001, 1001001006, 1002001000, 1003000002,
            1004002003, 1005002001, 1006001009, 1007001007, 1000002002, 1001003003,
            1002003005, 1003001001, 1004000000, 1005000003, 1006003005, 1007003008,
            1000000002, 1001002004, 1002002009, 1003003000, 1004001003, 1005001007,
            1006002006, 1007002007, 1000001003, 1001000007, 1002000000, 1003002004,
            1004003007, 1005003006, 1006000000, 1007000004, 1000003002, 1001001007,
            1002001001, 1003000003], device='cuda:0')
    ```
    
    Reviewed By: CRobeck
    
    Differential Revision: D76852464
    
    fbshipit-source-id: 4d74a4c9be844cd1dc516dee3dba4b7591fdfb14

--- tritonbench/kernels/profile.py
+++ tritonbench/kernels/profile.py
@@ -30,3 +30,54 @@ def time(_semantic=None):
         return tl.extra.cuda.globaltimer()
     else:
         tl.static_assert(False, "Unsupported platform")
+
+
+@triton.jit
+def smid(_semantic=None):
+    if IS_HIP:
+        # Reference: https://github.com/triton-lang/triton/blob/4d3c4980044eb40fae31490ee07fcf94b6bb7f4a/third_party/amd/backend/include/hip/amd_detail/amd_device_functions.h#L891-L937
+        HW_ID_CU_ID_OFFSET: tl.constexpr = 8
+        HW_ID_CU_ID_SIZE: tl.constexpr = 4
+        cu_id = tl.inline_asm_elementwise(
+            f"""
+            s_getreg_b32 $0, hwreg(HW_REG_HW_ID, {HW_ID_CU_ID_OFFSET}, {HW_ID_CU_ID_SIZE})
+            """,
+            "=s",
+            [],
+            dtype=tl.int32,
+            is_pure=False,
+            pack=1,
+        )
+
+        HW_ID_SE_ID_OFFSET: tl.constexpr = 13
+        HW_ID_SE_ID_SIZE: tl.constexpr = 3
+        se_id = tl.inline_asm_elementwise(
+            f"""
+            s_getreg_b32 $0, hwreg(HW_REG_HW_ID, {HW_ID_SE_ID_OFFSET}, {HW_ID_SE_ID_SIZE})
+            """,
+            "=s",
+            [],
+            dtype=tl.int32,
+            is_pure=False,
+            pack=1,
+        )
+
+        XCC_ID_XCC_ID_OFFSET: tl.constexpr = 0
+        XCC_ID_XCC_ID_SIZE: tl.constexpr = 4
+        xcc_id = tl.inline_asm_elementwise(
+            f"""
+            s_getreg_b32 $0, hwreg(HW_REG_XCC_ID, {XCC_ID_XCC_ID_OFFSET}, {XCC_ID_XCC_ID_SIZE})
+            """,
+            "=s",
+            [],
+            dtype=tl.int32,
+            is_pure=False,
+            pack=1,
+        )
+
+        # Format: 1 | 3 digits for xcc_id | 3 digits for se_id | 3 digits for cu_id
+        return 1_000_000_000 + xcc_id * 1_000_000 + se_id * 1_000 + cu_id
+    elif IS_CUDA:
+        return tl.extra.cuda.smid()
+    else:
+        tl.static_assert(False, "Unsupported platform")
