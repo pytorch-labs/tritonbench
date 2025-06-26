462bde31dcffffa0536289ba9481c4f381ac641b
Xu Zhao <xzhao9@meta.com>
2025-06-26 15:14:39 -0700

    Add tflops to grouped_gemm (#254)
    Summary:
    Pull Request resolved: https://github.com/pytorch-labs/tritonbench/pull/254
    
    Add actual `tflops` metrics to grouped_gemm operator
    
    Reviewed By: njriasan
    
    Differential Revision: D77385029
    
    fbshipit-source-id: f7770af6146d5c1dd1ce7001706a0dea6140476a

--- tritonbench/operators/grouped_gemm/operator.py
+++ tritonbench/operators/grouped_gemm/operator.py
@@ -1,13 +1,10 @@
-from typing import Callable, Generator, List
+from typing import Any, Generator
 
 import torch
-import triton
-import triton.language as tl
 
 from tritonbench.utils.triton_op import (
     BenchmarkOperator,
     BenchmarkOperatorMetrics,
-    Mode,
     register_benchmark,
     register_metric,
 )
@@ -45,6 +42,18 @@ class Operator(BenchmarkOperator):
                 group_A.append(A)
                 group_B.append(B)
             yield group_A, group_B
+
+    @register_metric()
+    def flops(
+        self, fn_name: str, example_inputs: Any, metrics: BenchmarkOperatorMetrics
+    ) -> float:
+        group_A, group_B = example_inputs
+        flops = 0
+        for a, b in zip(group_A, group_B):
+            m, k = a.size()
+            k, n = b.size()
+            flops += m * k * 2 * n
+        return flops
 
     def get_x_val(self, example_inputs):
         N = example_inputs[0][0].shape[0]
