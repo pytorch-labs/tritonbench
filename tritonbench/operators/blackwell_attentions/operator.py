9868a3ebfe72609b9ba12633c635ba32ae21d21d
Manman Ren <mren@meta.com>
2025-07-07 14:54:07 -0700

    Add Gluon FA Blackwell
    Summary: Copy python/tutorials/gluon/01-attention-forward.py from OSS
    Version from PR7238
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- tritonbench/operators/blackwell_attentions/operator.py
+++ tritonbench/operators/blackwell_attentions/operator.py
@@ -20,6 +20,10 @@ from tritonbench.kernels.triton_fused_attention import (
     attention_opt as triton_tutorial_FA2_opt,
 )
 
+from tritonbench.kernels.gluon_attention_forward import (
+    attention_forward as gluon_blackwell_fwd,
+)
+
 from tritonbench.utils.env_utils import get_nvidia_gpu_model, is_cuda
 
 # [Optional] flash_attn v2
@@ -284,6 +288,18 @@ class Operator(BenchmarkOperator):
     ) -> Callable:
         return lambda: triton_tutorial_FA2_opt(
             q, k, v, self.causal, self.sm_scale, "tma_ws_persistent_blackwell"
+        )
+
+    # Only works with triton main, forward only.
+    @register_benchmark(enabled=False)
+    def gluon_blackwell_tutorial_fwd(
+        self,
+        q: torch.Tensor,
+        k: torch.Tensor,
+        v: torch.Tensor,
+    ) -> Callable:
+        return lambda: gluon_blackwell_fwd(
+            q, k, v, self.causal, self.sm_scale
         )
 
     @register_metric(x_only=True)
