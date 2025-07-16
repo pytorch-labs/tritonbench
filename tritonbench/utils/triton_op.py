fc258d3d1906f0dcd61f043ecf8a046264c6f431
drisspg <drisspguessous@gmail.com>
2025-07-16 10:37:51 -0700

    Add more options to triton bench
    stack-info: PR: https://github.com/pytorch-labs/tritonbench/pull/292, branch: drisspg/stack/1

--- tritonbench/utils/triton_op.py
+++ tritonbench/utils/triton_op.py
@@ -1439,6 +1439,8 @@ class BenchmarkOperator(metaclass=PostInitProcessor):
                 self.dump_ir(input_id, fn)
         except torch.cuda.OutOfMemoryError:
             metrics.error_msg = "CUDA OOM"
+        except NotImplementedError:
+            metrics.error_msg = "not supported"
         except Exception as e:
             if not self.tb_args.keep_going:
                 raise
